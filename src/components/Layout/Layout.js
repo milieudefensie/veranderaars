fdc29fac1b9269a09d9c9c8cfa8f80e4e01c8a13
Rodrigo Leon <rodrigo@tectonica.co>
2024-01-25 09:43:02 -0300

    Tally script added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Footer from './Footer/Footer';
 
@@ -6,6 +6,34 @@ import '../../styles/main.scss';
 
 function Layout({ children, bgColor = null, extraClassNames = null, heroBgColor = 'default' }) {
   const [navOpen, setNavOpen] = useState(false);
+
+  useEffect(() => {
+    const trackingTally = document.createElement('script');
+    trackingTally.src = 'https://tally.so/widgets/embed.js';
+    trackingTally.async = true;
+    document.body.appendChild(trackingTally);
+
+    const tallyConfig = document.createElement('script');
+    tallyConfig.innerHTML = `
+      window.TallyConfig = {
+        "formId": "mZaLJA",
+        "popup": {
+          "width": 300,
+          "hideTitle": true,
+          "open": {
+            "trigger": "scroll",
+            "scrollPercent": 50
+          }
+        }
+      };
+    `;
+    document.body.appendChild(tallyConfig);
+
+    return () => {
+      document.body.removeChild(trackingTally);
+      document.body.removeChild(tallyConfig);
+    };
+  }, []);
 
   return (
     <>
