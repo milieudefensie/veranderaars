9f738ce37d452d50f90f59a1f4a7d63c6a58ddb3
Rodrigo Leon <rodrigo@tectonica.co>
2024-03-14 13:02:56 -0300

    test

--- src/components/Blocks/HubspotForm/HubspotForm.js
+++ src/components/Blocks/HubspotForm/HubspotForm.js
@@ -28,6 +28,7 @@ const HubspotForm = ({ id, formId, region, portalId, style = 'default' }) => {
             },
             onFormReady: (ctx) => {
               const { id } = ctx;
+              return;
 
               // Handlers
               const inputs = document.querySelectorAll(`#${id} .hs-input`);
