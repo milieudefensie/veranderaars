8339b6f1a77ffd1fdded1306a8f43d95b68aa32b
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-01-28 11:25:23 -0300

    CSL Integrations: new service

--- src/api/csl-handler/index.js
+++ src/api/csl-handler/index.js
@@ -1,34 +1,59 @@
 import axios from 'axios';
 import trackRequest from '../_utils/track-request';
 
-export default async function handler(req, res) {
-  let successRequest = false;
+const WEBHOOK_ENDPOINTS = {
+  USER: 'https://api-eu1.hubapi.com/automation/v4/webhook-triggers/139720471/YfoJAHB',
+  EVENT: 'https://api-eu1.hubapi.com/automation/v4/webhook-triggers/139720471/wNTGt4x',
+};
+const USER_CASES = [
+  'user.created',
+  'user.updated',
+  'attendee.created',
+  'attendee.updated',
+  'attendee.deleted',
+  'attendee.confirmed',
+  'unsubscribe.created',
+];
+const EVENT_CASES = ['event.created', 'event.updated'];
 
-  const cslInfo = req.body;
+const getWebhookEndpoint = (type) => {
+  if (USER_CASES.includes(type)) return WEBHOOK_ENDPOINTS.USER;
+  if (EVENT_CASES.includes(type)) return WEBHOOK_ENDPOINTS.EVENT;
+  return null;
+};
+
+export default async function handler(req, res) {
+  const { type, ...rest } = req.body;
+  const webhookEndpoint = getWebhookEndpoint(type);
+
+  if (!webhookEndpoint) {
+    return res.status(400).json({ message: 'Invalid CSL type' });
+  }
+
   const cslUpdatedInfo = {
-    ...cslInfo,
+    ...rest,
     fakeEmail: 'donotremove@formio.integration.com',
   };
 
-  await axios
-    .post(`https://api-eu1.hubapi.com/automation/v4/webhook-triggers/139720471/wNTGt4x`, cslUpdatedInfo)
-    .then((response) => {
-      successRequest = true;
-      res.status(200).json({ message: `OK. Data: ${JSON.stringify(cslUpdatedInfo)}` });
-      return;
-    })
-    .catch((error) => {
-      successRequest = false;
-      console.log('Error:', error);
-      res.status(400).json({ message: error.message });
-      return;
-    })
-    .finally(async () => {
-      await trackRequest({
-        date: new Date().toISOString(),
-        endpoint: 'CSL Handler / https://api-eu1.hubapi.com/automation/v4/webhook-triggers/139720471/wNTGt4x',
-        body: cslUpdatedInfo,
-        success: successRequest,
-      });
+  try {
+    await axios.post(webhookEndpoint, cslUpdatedInfo);
+
+    await trackRequest({
+      date: new Date().toISOString(),
+      endpoint: webhookEndpoint,
+      body: cslUpdatedInfo,
+      success: true,
     });
+
+    return res.status(200).json({ message: `OK. Data: ${JSON.stringify(cslUpdatedInfo)}` });
+  } catch (error) {
+    await trackRequest({
+      date: new Date().toISOString(),
+      endpoint: webhookEndpoint,
+      body: cslUpdatedInfo,
+      success: false,
+    });
+
+    return res.status(400).json({ message: error.message });
+  }
 }
