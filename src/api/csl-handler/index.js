b0371c891e56cee669278d2dca84baacc2987e37
marceltectonica <110401825+marceltectonica@users.noreply.github.com>
2025-01-29 07:30:41 +0100

    Update index.js
    bad response should also be 200

--- src/api/csl-handler/index.js
+++ src/api/csl-handler/index.js
@@ -55,6 +55,6 @@ export default async function handler(req, res) {
       success: false,
     });
 
-    return res.status(400).json({ message: error.message });
+    return res.status(200).json({ message: error.message });
   }
 }
