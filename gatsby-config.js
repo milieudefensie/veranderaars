03a4460e4ff6ada347b54252929f7b2012832348
Fernando Petroli <fernando@tectonica.co>
2025-02-26 11:06:13 -0300

    add redirect from old pdf file version

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,27 @@ module.exports = {
     'gatsby-plugin-sass',
     'gatsby-plugin-advanced-sitemap',
     'gatsby-plugin-react-helmet',
-    `gatsby-plugin-netlify`,
+    // `gatsby-plugin-netlify`,
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        headers: {}, // Define headers here if needed
+        // Define your redirects here
+        redirects: [
+          {
+            fromPath: 'https://www.datocms-assets.com/115430/1739453560-algemene-ava-handleiding-md-2025.pdf',
+            toPath: 'https://www.datocms-assets.com/115430/1740411395-algemene-ava-handleiding-md-2025.pdf',
+            isPermanent: true, // Creates a 301 redirect (permanent)
+            force: true, // Ensures the redirect happens even if the fromPath exists
+          },
+          {
+            fromPath: 'https://www.datocms-assets.com/115430/1740481214-algemene-ava-handleiding-md-2025.pdf',
+            toPath: 'https://www.datocms-assets.com/115430/1740411395-algemene-ava-handleiding-md-2025.pdf',
+            isPermanent: true, // Creates a 301 redirect (permanent)
+            force: true, // Ensures the redirect happens even if the fromPath exists
+          },
+        ],
+      },
+    },
   ],
 };
