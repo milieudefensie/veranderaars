ca65808bed194ebae50d868f24adf9f8f1816c4a
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-03-19 10:38:59 -0300

    Embed block: fix case with scripts

--- src/components/Blocks/EmbedIframe/EmbedIframe.js
+++ src/components/Blocks/EmbedIframe/EmbedIframe.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './index.scss';
 
 function EmbedIframe({ content }) {
-  return <div className="embed-iframe" dangerouslySetInnerHTML={{ __html: content.iframeCode }} />;
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const scripts = containerRef.current.getElementsByTagName('script');
+      for (let script of scripts) {
+        const newScript = document.createElement('script');
+        newScript.src = script.src;
+        newScript.async = true;
+        document.body.appendChild(newScript);
+      }
+    }
+  }, [content]);
+
+  return <div ref={containerRef} className="embed-iframe" dangerouslySetInnerHTML={{ __html: content.iframeCode }} />;
 }
 
 export default EmbedIframe;
