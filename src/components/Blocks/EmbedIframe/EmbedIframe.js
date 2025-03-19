a02bd0bb4bdfca2f31f82ca3ec71d97088ee1f0a
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-03-19 10:49:44 -0300

    QA Fix to avoid duplicated scripts

--- src/components/Blocks/EmbedIframe/EmbedIframe.js
+++ src/components/Blocks/EmbedIframe/EmbedIframe.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './index.scss';
 
 function EmbedIframe({ content }) {
   const containerRef = useRef(null);
+  const [scriptCreated, setScriptCreated] = useState(false);
 
   useEffect(() => {
     if (containerRef.current) {
       const scripts = containerRef.current.getElementsByTagName('script');
       for (let script of scripts) {
-        if (script.src && !document.querySelector(`script[src="${script.src}"]`)) {
+        console.log({ src: script.src, exist: document.querySelector(`script[src="${script.src}"]`) });
+
+        if (script.src && scriptCreated === false) {
           const newScript = document.createElement('script');
           newScript.src = script.src;
           newScript.async = true;
           document.body.appendChild(newScript);
+          setScriptCreated(true);
         }
       }
     }
