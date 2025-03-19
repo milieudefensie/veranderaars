a9d2081c5f582fd592dff2a05638f9947717f699
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-03-19 10:54:24 -0300

    Test

--- src/components/Blocks/EmbedIframe/EmbedIframe.js
+++ src/components/Blocks/EmbedIframe/EmbedIframe.js
@@ -3,20 +3,18 @@ import './index.scss';
 
 function EmbedIframe({ content }) {
   const containerRef = useRef(null);
-  const [scriptCreated, setScriptCreated] = useState(false);
+  // const [scriptCreated, setScriptCreated] = useState(false);
 
   useEffect(() => {
     if (containerRef.current) {
       const scripts = containerRef.current.getElementsByTagName('script');
       for (let script of scripts) {
-        console.log({ src: script.src, exist: document.querySelector(`script[src="${script.src}"]`) });
-
-        if (script.src && scriptCreated === false) {
+        if (script.src && !document.querySelector(`script[src="${script.src}"]`)) {
           const newScript = document.createElement('script');
           newScript.src = script.src;
           newScript.async = true;
           document.body.appendChild(newScript);
-          setScriptCreated(true);
+          // setScriptCreated(true);
         }
       }
     }
