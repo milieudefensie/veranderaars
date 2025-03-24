fc68971c6d204a61fae16ad9687c9effb21e148e
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-03-24 08:10:36 -0300

    Embed QA Fixes

--- src/components/Blocks/EmbedIframe/EmbedIframe.js
+++ src/components/Blocks/EmbedIframe/EmbedIframe.js
@@ -1,25 +1,53 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './index.scss';
 
 function EmbedIframe({ content }) {
   const containerRef = useRef(null);
+  const [addAsHTML, setAddAsHtml] = useState(false);
 
   useEffect(() => {
-    if (containerRef.current) {
-      const scripts = containerRef.current.getElementsByTagName('script');
-      for (let script of scripts) {
-        if (script.src && !document.querySelector(`script[src="${script.src}"]`)) {
-          const newScript = document.createElement('script');
-          newScript.src = script.src;
-          newScript.async = true;
-          document.body.appendChild(newScript);
-          console.log(`Added script: ${script.src}`);
-        }
-      }
+    if (!containerRef.current) return;
+
+    const contentHasEventixScript = content.iframeCode.includes('https://shop.eventix.io/build/integrate.js');
+
+    if (!contentHasEventixScript) {
+      setAddAsHtml(true);
+      return;
+    }
+
+    containerRef.current.innerHTML = '';
+
+    // Crear e insertar el div con el ID requerido por Eventix
+    const eventixIdFromScript = content.iframeCode.match(/data-url="([^"]+)"/)[1];
+
+    const div = document.createElement('div');
+    div.id = 'shop-frame';
+    div.style.maxWidth = '600px';
+    div.style.margin = '0 auto';
+    div.setAttribute('data-url', eventixIdFromScript);
+
+    containerRef.current.appendChild(div);
+
+    if (!document.querySelector('script[src="https://shop.eventix.io/build/integrate.js"]')) {
+      const script = document.createElement('script');
+      script.src = 'https://shop.eventix.io/build/integrate.js';
+      script.async = true;
+      script.onload = () => {
+        console.log('Script de Eventix cargado y ejecutado');
+      };
+      document.body.appendChild(script);
+    } else {
+      console.log('Script de Eventix ya estaba cargado.');
     }
   }, [content]);
 
-  return <div ref={containerRef} className="embed-iframe" dangerouslySetInnerHTML={{ __html: content.iframeCode }} />;
+  return (
+    <div
+      ref={containerRef}
+      className="embed-iframe"
+      dangerouslySetInnerHTML={{ __html: addAsHTML ? content.iframeCode : null }}
+    />
+  );
 }
 
 export default EmbedIframe;
