d30a1c30ea55be8d12569a8812beb400806715ce
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-05-20 10:50:14 -0300

    Blog signal: disable protection temporary

--- src/components/Blocks/BlockGroupsSignal/BlockGroupsSignal.jsx
+++ src/components/Blocks/BlockGroupsSignal/BlockGroupsSignal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, navigate, useStaticQuery } from 'gatsby';
 import { BotProtectionProvider, BotProtectionStatus, ProtectedLink } from '../../Global/BotProtection/BotProtection';
 
 import './styles.scss';
@@ -29,7 +29,14 @@ const BlockGroupsSignal = ({ block }) => {
           {signalGroups.nodes.map((group) => {
             return (
               <div className="col-md-6 mb-3">
-                <ProtectedLink to={group.signalChat} className="custom-btn custom-btn-primary w-100">
+                <ProtectedLink
+                  to={group.signalChat}
+                  className="custom-btn custom-btn-primary w-100"
+                  // TODO: Disable before production
+                  onClick={() => {
+                    navigate(group.signalChat);
+                  }}
+                >
                   {group.title}
                 </ProtectedLink>
               </div>
