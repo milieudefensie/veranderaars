47e873716fb06f1e41093110e54c359941e38f5e
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-09-23 08:04:21 -0300

    Add flag for travel together tool + fix locality in event card

--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -44,35 +44,7 @@ export const HomeQuery = graphql`
       }
     }
     cslHighlightEvent: externalEvent(slug: { eq: $cslHighlightedEvent }) {
-      __typename
-      id: slug
-      slug
-      title
-      description
-      start_at
-      end_at
-      raw_start
-      raw_end
-      image_url
-      labels
-      start_in_zone
-      end_in_zone
-      location {
-        latitude
-        longitude
-        venue
-        query
-        region
-      }
-      calendar {
-        name
-        slug
-      }
-      hiddenAddress
-      additional_image_sizes_url {
-        url
-        style
-      }
+      ...CSLEventCard
     }
     page: datoCmsHome {
       seo: seoMetaTags {
