0a6368c6a1325d57a8e0a9a6d15c72560185f195
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-10-21 08:47:31 -0300

    Navigation: QA Visual fixes

--- src/components/Global/Nav/nav.tsx
+++ src/components/Global/Nav/nav.tsx
@@ -176,7 +176,7 @@ const MobileNavbar = (
                       <path
                         d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
                         stroke="#000000"
-                        strokeWidth="1.5"
+                        strokeWidth="2.5"
                         strokeLinecap="round"
                         strokeLinejoin="round"
                       />
